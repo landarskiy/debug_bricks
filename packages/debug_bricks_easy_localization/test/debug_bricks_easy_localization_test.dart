bb46da41c0f9ddafa09a522e05a0c06898ec368b
Yauhen Landarski <landarskiy@gmail.com>
2022-08-04 11:01:48 +0200

    UI tests

--- packages/debug_bricks_easy_localization/test/debug_bricks_easy_localization_test.dart
+++ packages/debug_bricks_easy_localization/test/debug_bricks_easy_localization_test.dart
@@ -1,16 +1,48 @@
 import 'package:debug_bricks_easy_localization/debug_bricks_easy_localization.dart';
+import 'package:debug_bricks_ui/debug_bricks_ui.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'utils/ui_utils.dart';
 
-void main() {
-  testWidgets('DeviceInfoBrick widget test', (tester) async {
-    await tester.pumpWidgetWithMaterial(
-      const EasyLocalizationBrick(
-        title: 'T',
-      ),
+late BuildContext _context;
+
+class ContextConsumer extends StatelessWidget {
+  const ContextConsumer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    _context = context;
+    return const EasyLocalizationBrick(
+      title: 'T',
     );
+  }
+}
+
+void main() {
+  testWidgets('EasyLocalizationBrick widget test', (tester) async {
+    await EasyLocalization.ensureInitialized();
+    await tester.pumpWidgetWithMaterial(
+      const ContextConsumer(),
+    );
+    _context.setLocale(localeEn);
     expect(find.text('T'), findsOneWidget);
-    //TODO finish test
+    expect(find.text('ru'), findsNothing);
+    expect(find.text('en'), findsOneWidget);
+    await tester.tap(find.byType(ExpandableBrick));
+    await tester.pumpAndSettle();
+    expect(find.text('ru'), findsOneWidget);
+    expect(find.text('en'), findsNWidgets(2));
+    _context.setLocale(localeRu);
+    await tester.pumpAndSettle();
+    expect(find.text('ru'), findsNWidgets(2));
+    expect(find.text('en'), findsOneWidget);
+  });
+
+  test('LocaleAdapter convert test', () {
+    const adapter = LocaleAdapter();
+    expect(adapter.convert(const Locale('en')), 'en');
+    expect(adapter.convert(const Locale('en', 'us')), 'en-us');
   });
 }
--- packages/debug_bricks_easy_localization/test/utils/ui_utils.dart
+++ packages/debug_bricks_easy_localization/test/utils/ui_utils.dart
@@ -1,9 +1,13 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+const localeEn = Locale('en');
+const localeRu = Locale('ru');
+
 extension WidgetTesterExtension on WidgetTester {
   Future<void> pumpWidgetWithMaterial(Widget widget) {
-    return pumpWidget(wrapMaterial(widget));
+    return pumpWidget(wrapEasyLocalization(wrapMaterial(widget)));
   }
 }
 
@@ -13,4 +17,32 @@ Widget wrapMaterial(Widget child) {
       body: child,
     ),
   );
+}
+
+Widget wrapEasyLocalization(Widget child) {
+  return EasyLocalization(
+    supportedLocales: const [localeEn, localeRu],
+    path: 'assets/translations',
+    saveLocale: false,
+    useOnlyLangCode: true,
+    assetLoader: const TestLoader(),
+    fallbackLocale: localeEn,
+    child: child,
+  );
+}
+
+class TestLoader extends AssetLoader {
+  const TestLoader();
+
+  @override
+  Future<Map<String, dynamic>> load(String fullPath, Locale locale) {
+    const Map<String, dynamic> en = {
+      'value': 'en',
+    };
+    const Map<String, dynamic> ru = {
+      'value': 'ru',
+    };
+    final Map<String, Map<String, dynamic>> mapLocales = {"en": en, "ru": ru};
+    return Future.value(mapLocales[locale.toString()]);
+  }
 }

ccd6641e38a1cf161e045373876ff672f4c12c84
Yauhen Landarski <landarskiy@gmail.com>
2022-08-05 11:35:26 +0200

    Optimize imports

--- packages/debug_bricks_ui/example/test/widget_test.dart
+++ packages/debug_bricks_ui/example/test/widget_test.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:example/main.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('App test', (WidgetTester tester) async {

e0237f7c4227c3dc75b3578666225cc3bab72340
Yauhen Landarski <landarskiy@gmail.com>
2022-08-25 10:50:48 +0200

    Add tests

--- packages/debug_bricks_theme_info/test/debug_bricks_theme_info_test.dart
+++ packages/debug_bricks_theme_info/test/debug_bricks_theme_info_test.dart
@@ -1,5 +1,6 @@
 import 'package:debug_bricks_theme_info/debug_bricks_theme_info.dart';
 import 'package:debug_bricks_theme_info/src/color_extension.dart';
+import 'package:debug_bricks_theme_info/src/rounded_label.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -34,13 +35,49 @@ void main() {
     expect(Colors.white.format(adaptiveHexColorFormatter), '#FFFFFF');
   });
 
-  testWidgets('ColorInfoBrick widget test', (tester) async {
+  testWidgets('ExpandedColorInfoBrick widget test', (tester) async {
     await tester.pumpWidgetWithMaterial(
-      ColorInfoBrick(
+      ExpandedColorInfoBrick(
         colorInfo: ColorInfo('N', Colors.white, Colors.red),
       ),
     );
     expect(find.text('N'), findsOneWidget);
     expect(find.text('#FFFFFF'), findsOneWidget);
+  });
+
+  testWidgets('ThemeColorInfoBrick widget test', (tester) async {
+    await tester.pumpWidgetWithMaterial(
+      const SingleChildScrollView(child: ThemeColorInfoBrick()),
+    );
+    expect(find.byType(RoundedLabel), findsNWidgets(6));
+
+    expect(find.text('Primary'), findsNWidgets(2));
+    expect(find.text('On Primary'), findsOneWidget);
+    expect(find.text('Primary Container'), findsOneWidget);
+    expect(find.text('On Primary Container'), findsOneWidget);
+
+    expect(find.text('Secondary'), findsNWidgets(2));
+    expect(find.text('On Secondary'), findsOneWidget);
+    expect(find.text('Secondary Container'), findsOneWidget);
+    expect(find.text('On Secondary Container'), findsOneWidget);
+
+    expect(find.text('Tertiary'), findsNWidgets(2));
+    expect(find.text('On Tertiary'), findsOneWidget);
+    expect(find.text('Tertiary Container'), findsOneWidget);
+    expect(find.text('On Tertiary Container'), findsOneWidget);
+
+    expect(find.text('Error'), findsNWidgets(2));
+    expect(find.text('On Error'), findsOneWidget);
+    expect(find.text('Error Container'), findsOneWidget);
+    expect(find.text('On Error Container'), findsOneWidget);
+
+    expect(find.text('Background'), findsOneWidget);
+    expect(find.text('On Background'), findsOneWidget);
+    expect(find.text('Surface'), findsOneWidget);
+    expect(find.text('On Surface'), findsOneWidget);
+
+    expect(find.text('Surface Variant'), findsOneWidget);
+    expect(find.text('On Surface Variant'), findsOneWidget);
+    expect(find.text('Outline'), findsNWidgets(2));
   });
 }
